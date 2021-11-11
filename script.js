fcb5f0f73e9fcebcece9291e11d27822f792655d
naiden <na4ko359@gmail.com>
2021-11-11 20:24:02 +0200

    ui for rps

--- script.js
+++ script.js
@@ -1,75 +1,69 @@
-let playerWins = 0;
-let computerWins = 0;
+let playerWins = 1;
+let computerWins = 1;
 
 function computerPlay(){
-    const choices = ["ROCK", "PAPER", "SCISSORS"];
-    return choices[Math.floor(Math.random()*choices.length)];                    
- }
- 
- 
- function playerPlay(){
-     const input = prompt("Whats your choice?");
-     return input.toLowerCase(); // if no answer is input, an error pops up: Uncaught TypeError: Cannot read properties of null ('toLowerCase')
- }        
- 
- 
- function playRound(playerSelection, computerSelection){
-     
-     switch(playerSelection){
-     case "rock":
-         if(computerSelection == "ROCK"){
-             console.log("It's a draw.");
-         } else if (computerSelection == "PAPER") {
-             console.log("Computer wins.");
-             computerWins++;
-             console.log("Computer: " + computerWins);
-         } else if (computerSelection == "SCISSORS") {
-             console.log("Player wins.");
-             playerWins++;
-             console.log("Player: " + playerWins);
-         }
-         break;
-     case "paper":
-         if (computerSelection == "ROCK") {
-             console.log("Player wins.");
-             playerWins++;
-             console.log("Player: " + playerWins);
-         } else if (computerSelection == "PAPER") {
-             console.log("It's a draw.");
-         } else if (computerSelection == "SCISSORS") {
-             console.log("Computer wins.");
-             computerWins++;
-             console.log("Computer: " + computerWins);
-         }
-         break;
-     case "scissors":
-         if (computerSelection == "ROCK") {
-             console.log("Computer wins.");
-             computerWins++;
-             console.log("Computer: " + computerWins);
-         } else if (computerSelection == "PAPER") {
-             console.log("Player wins.");
-             playerWins++;
-             console.log("Player: " + playerWins);
-         } else if (computerSelection == "SCISSORS") {
-             console.log("It's a draw.");
-         }
-         break;
- }                
- }   
- 
-function gameOfFive() { 
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = computerPlay();  
-        const playerSelection = playerPlay();
-        console.log(playRound(playerSelection, computerSelection));   
-    }    
-    if (playerWins > computerWins) {
-        console.log("Player has won the battle");
-    } else if (computerWins > playerWins) {
-        console.log("Computer has won the battle");
-    } else if (playerWins == computerWins){
-        console.log("The game ends in a draw");
-    }
+const choices = ["rock", "paper", "scissors"];
+return choices[Math.floor(Math.random()*choices.length)];                    
 }
-gameOfFive();
+
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", function() {
+computerPlay();
+const computerSelection = computerPlay(); 
+playRound("rock", computerSelection);
+});
+
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", function() {
+computerPlay();
+const computerSelection = computerPlay(); 
+playRound("paper", computerSelection);
+});
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", function() {     
+computerPlay();
+const computerSelection = computerPlay(); 
+playRound("scissors", computerSelection);
+});
+
+const statsContainer = document.querySelector(".stats");
+const div = document.createElement("div");
+statsContainer.appendChild(div);
+
+function playRound(playerSelection, computerSelection){ 
+     if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+            const pWin = document.createElement("div");
+            const pWins = document.createElement("p");
+            pWins.textContent = `Player wins + ${playerWins++}`;
+            div.appendChild(pWins);
+            console.log("Player wins");
+            if (playerWins == 6) {
+                pWins.textContent = "Player has 5 points and he wins the game. The game will reset in 3 seconds.";
+                div.appendChild(pWins);
+                setTimeout(function () { location.reload(true); }, 3000);
+                
+            }
+    } else if ((playerSelection =="rock" && computerSelection == "paper") || 
+    (playerSelection == "paper" && computerSelection == "scissors") || 
+    (playerSelection == "scissors" && computerSelection == "rock")) { 
+        const cWin = document.createElement("div");
+        const cWins = document.createElement("p");
+        cWins.textContent = `Computer wins + ${computerWins++}`;
+        div.appendChild(cWins);
+        console.log("Computer wins");
+        if (computerWins == 6) {
+            cWins.textContent = "Computer has 5 points and he wins the game. The game will reset in 3 seconds.";
+            div.appendChild(cWins);
+            setTimeout(function () { location.reload(true); }, 3000);   
+        }
+    } else {
+        const draw = document.createElement("div");
+        const draws = document.createElement("p");
+        draws.textContent = "It's a draw";
+        div.appendChild(draws);
+        console.log("It's a draw");
+    }         
+ }
